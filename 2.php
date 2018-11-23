83f9b58b2035921246fc75bbd445f91652057772
FTC5 <dan.m1512@gmail.com>
2018-11-23 13:10:05 +0200

    Create 2.php

---
+++ 2.php
@@ -0,0 +1,72 @@
+<?php
+	echo '<style>
+		div.wrapper {
+			width:1050px;
+			height:480px;
+		}
+		div.left_block {
+			float:left;
+			width:640px;
+			height:480px;
+		}
+		div.right_block {
+			float:right;
+			width:400px;
+			height:480px;
+		}
+  	</style>';
+
+echo'	<div class="wrapper">
+		<div class="right_block">
+			<div>
+				<span><h1>"Стол Loras дуб/черный"</h1></span>
+			</div>
+			<div>
+				<p>В наявності</p>
+				<p>Код: LORASDC150</p>
+				<p><b>Ціна:</b> 7&nbsp;844&nbsp; грн. </p>
+			</div>
+		</div>';
+echo'	<div class="left_block">
+			<img class="b-product-image__img" src="https://images.ua.prom.st/222879461_w500_h500_lorasdc150_st____db_czarny.jpg" data-subscribe="click: showImage@ID-e1a54bdc-d4e5-4f25-a793-91a1113b5f9d&quot;222879461&quot;" style="width:640px;height:480px">
+		</div>
+	</div>'
+	
+echo'	<details>
+		<summary>Опис товару</summary>
+		<div class="b-user-content b-product__hidden-block"><h2>"Стол Loras дуб/черный"</h2>
+			<p><strong><span style="font-size:14.0pt">Стіл </span><span style="font-size:14.0pt">Loras</span></strong><span style="font-size:14.0pt"> </span><span style="font-size:14.0pt">– цікавий варіант в сучасному нестандартному дизайні. Опорою гарнітура служить міцний каркас чорного забарвлення з металу. Досить незвичайна конструкція, що гарантує стійкість і надійність. Стільниця високої якості виконана з натурального дуба з ефектною деревної текстурою поверхні. Оригінальне компонування різних матеріалів і квітів. Такі меблі довгострокова в експлуатації. Стильний зовнішній вигляд прикрасить сучасний інтерєр, додаючи нотку комфорту. До столу підійдуть стільчики </span><span style="font-size:14.0pt">Enzo</span><span style="font-size:14.0pt">. Параметры (шир/длин/выс) – 90х150х75 см.</span></p>
+			<p>Стільниця: натуральний дуб</p>
+			<p><strong>Каркас:</strong> метал</p>
+			<p><strong>Довжина:</strong> 150 см</p>
+			<p><strong>Ширина:</strong> 90 см</p>
+			<p><strong>Висота:</strong> 75 см</p>
+			<p><img alt="" src=" "style="width:320px;height:240px"></p>
+		</div>
+	</details>'
+echo'	<details>
+		<summary>Характеристики</summary>
+		<div class="b-product__hidden-block">
+			<table class="b-product-info">
+				<tbody>
+					<tr>
+						<th class="b-product-info__header" colspan="2">Основні</th></tr>
+					<tr>
+						<td class="b-product-info__cell">Виробник</td><td class="b-product-info__cell">Signal</td></tr>
+					<tr>
+						<td class="b-product-info__cell">Країна виробника</td><td class="b-product-info__cell">Польша</td></tr>
+				</tbody>
+			</table>
+		</div>
+	</details>'
+echo'	<details>
+		<summary>Інформація для замовлення</summary>
+		<div class="b-product__hidden-block">
+			<ul class="b-order-info">
+				<li class="b-order-info__item"><b>Ціна:</b> 7&nbsp;844&nbsp; грн.</li>
+				<li class="b-order-info__item"><b>Спосіб упаковки:</b> Товар доставляється до покупця в розібраному вигляді. Всі товари упаковані в картон з пінопласту захистом всередині.</li>
+			</ul>
+		</div>
+	</details>'
+
+?>
